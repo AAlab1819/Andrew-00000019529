1b931109a775ad62654a78019360cc828288b552
ndrewwjy <42855284+ndrewwjy@users.noreply.github.com>
2018-09-20 22:05:54 +0700

    Update 148A.cpp

--- Week03-Complexity/148A.cpp
+++ Week03-Complexity/148A.cpp
@@ -6,11 +6,12 @@ int main(){
 	
 	cin>>k>>l>>m>>n>>d;
 	
-	if(k!=1||l!=1||m!=1||n!=1)
-	for(int i=0;i<d;i++)
-		if(i%k==0||i%l==0||i%m==0||i%n==0)
-			poorDragon++;
-	
+	if(k!=1&&l!=1&&m!=1&&n!=1)
+	{
+		for(int i=0;i<d;i++)
+			if(i%k==0||i%l==0||i%m==0||i%n==0)
+				poorDragon++;
+	}
 	else
 		poorDragon=d;
 		
