db9572574f4e44891dcc66bf91838ca5a24e0e08
ndrewwjy <42855284+ndrewwjy@users.noreply.github.com>
2018-10-18 22:23:10 +0700

    Update Heaps Find the Running Median.cpp

--- Week06-HeapTree/Heaps Find the Running Median.cpp
+++ Week06-HeapTree/Heaps Find the Running Median.cpp
@@ -1,56 +1,39 @@
 #include<iostream> 
 using namespace std; 
 
-// C++ program for implementation of Heap Sort 
-#include <iostream> 
-  
-using namespace std; 
-  
-// To heapify a subtree rooted with node i which is 
-// an index in arr[]. n is size of heap 
 void heapify(float arr[], int n, int i) 
 { 
     int largest = i; // Initialize largest as root 
     int l = 2*i + 1; // left = 2*i + 1 
     int r = 2*i + 2; // right = 2*i + 2 
   
-    // If left child is larger than root 
     if (l < n && arr[l] > arr[largest]) 
         largest = l; 
   
-    // If right child is larger than largest so far 
     if (r < n && arr[r] > arr[largest]) 
         largest = r; 
   
-    // If largest is not root 
     if (largest != i) 
     { 
         swap(arr[i], arr[largest]); 
   
-        // Recursively heapify the affected sub-tree 
         heapify(arr, n, largest); 
     } 
 } 
-  
-// main function to do heap sort 
+   
 void heapSort(float arr[], int n) 
 { 
-    // Build heap (rearrange array) 
     for (int i = n / 2 - 1; i >= 0; i--) 
         heapify(arr, n, i); 
   
-    // One by one extract an element from heap 
     for (int i=n-1; i>=0; i--) 
     { 
-        // Move current root to end 
         swap(arr[0], arr[i]); 
   
-        // call max heapify on the reduced heap 
         heapify(arr, i, 0); 
     } 
 } 
-  
-/* A utility function to print array of size n */
+
 void printArray(float arr[], int n) 
 { 
     for (int i=0; i<n; ++i) 
@@ -64,21 +47,20 @@ int main()
 	
 	cin>>n;
 	
-	float a[n],med[n];
+	float numb[n],med[n];
 	
 	for(int i=0;i<n;i++)
 	{
-		cin>>a[i];
-		heapSort(a,i+1);
+		cin>>numb[i];
+		heapSort(numb,i+1);
 		
 		if((i+1)%2==1)
-			med[i]=a[i/2];
+			med[i]=numb[i/2];
 		else
-			med[i]=(a[i/2]+a[(i/2)+1])/2;
+			med[i]=(numb[i/2]+numb[(i/2)+1])/2;
 	}
 	cout<<endl;
 	printArray(med,n);
 }
   
-
 
